d13fcde51f002f5f68fa59cdf7cb5d1b0719d645
Jake Barnby <j.barnby@we-are-mea.com>
2020-12-03 21:27:25 +1300

    Fix options, remove redundant readonly

--- lib/fastlane/plugin/xamversion/options.rb
+++ lib/fastlane/plugin/xamversion/options.rb
@@ -12,13 +12,6 @@ module Xamversion
                                      description: "App build number value",
                                      optional: true),
 
-        FastlaneCore::ConfigItem.new(key: :readonly,
-                                     env_name: "xamversion_READONLY",
-                                     description: "Only print out current version and build",
-                                     default_value: false,
-                                     is_string: false,
-                                     optional: true),
-
         FastlaneCore::ConfigItem.new(key: :bump,
                                      description: "Interactively bump version number",
                                      default_value: false,
@@ -26,27 +19,27 @@ module Xamversion
                                      optional: true),
 
         FastlaneCore::ConfigItem.new(key: :platform,
-                                     env_name: "xamversion_PLATFORM",
+                                     env_name: "XAMVERSION_PLATFORM",
                                      description: "Targeted device platform (i.e. android, ios, osx)",
                                      optional: false),
 
         FastlaneCore::ConfigItem.new(key: :solution_path,
-                                     env_name: "xamversion_SOLUTION_PATH",
+                                     env_name: "XAMVERSION_SOLUTION_PATH",
                                      description: "Path to the build solution (sln) file",
                                      optional: true),
 
         FastlaneCore::ConfigItem.new(key: :project_path,
-                                     env_name: "xamversion_PROJECT_PATH",
+                                     env_name: "XAMVERSION_PROJECT_PATH",
                                      description: "Path to the build project (csproj) file",
                                      optional: true),
 
         FastlaneCore::ConfigItem.new(key: :manifest_path,
-                                     env_name: "xamversion_ANDROID_MANIFEST_PATH",
+                                     env_name: "XAMVERSION_ANDROID_MANIFEST_PATH",
                                      description: "Path to the android manifest (xml) file",
                                      optional: true),
 
         FastlaneCore::ConfigItem.new(key: :plist_path,
-                                     env_name: "xamversion_IOS_PLIST_PATH",
+                                     env_name: "XAMVERSION_IOS_PLIST_PATH",
                                      description: "Path to the iOS plist file",
                                      optional: true)
       ]
